4dacff1356b4a7a7d61cd961ec6c6eb599a89092
ly1998117 <liu.yang.mine@std.uestc.edu.cn>
2024-03-11 18:32:30 +0800

    update github link

--- interface.py
+++ interface.py
@@ -6,6 +6,14 @@ patients = ['马平社', '谢春艳', '邵佳南', '周庆玉', '何德翠', '�
 top_k = 10
 bottom_k = 10
 max_k = 20
+github = """
+               <div style="display: flex; align-items: center; justify-content: center; height: 100px;">
+                    <a href='https://github.com/ly1998117/MMCBM' target='_blank' style="text-decoration: none; color: black; text-align: center;">
+                        <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' width='50' height='50' style="display: block; margin: 0 auto;"/>
+                        GitHub repository
+                    </a>
+                </div>
+           """
 texts = [
     {'en': '# Intervention Interface', 'cn': '# 干预界面'},
     {'en': "### Prediction: Upload Fundus Images, Click Predict button to get the Top-10 concepts and prediction. \n"
@@ -17,15 +25,11 @@ texts = [
     {'en': 'Predict', 'cn': '预测'},
     {'en': 'Intervene Concept', 'cn': '干预概念'},
     {'en': 'Generate Report', 'cn': '生成报告'},
+    {'en': github, 'cn': github},
 ]
-
 name = gr.Text(label='Name', min_width=2, scale=1)
 pathology = gr.Text(label='Pathology', min_width=2, scale=1)
-# fa_e_label = gr.HTML(
-#     value="<div style='position: relative;top: -1px;'>"
-#           "<span style='position: absolute; left: 0; top: 0; padding: 0 10px; font-size: 14px; color: #6b727f; z-index:10;"
-#           "border-radius: 5px;border: 1px solid #e6e6e6; background-color:white'>FA-E</span>"
-#           "</div>")
+
 fa_e_label = gr.HTML(
     value="<div style='position: relative;top: -2px;'>"
           "<span style='position: absolute; left: 0; top: 0; padding: 0px; font-size: 14px; color: #6b727f;'>FA-E</span>"
@@ -88,23 +92,30 @@ def update_texts(language):
 
 
 with gr.Blocks() as demo:
-    title = gr.Markdown(texts[0][predict.language])
     with gr.Row():
-        with gr.Column(scale=50):
+        with gr.Column(scale=100000):
+            title = gr.Markdown(texts[0][predict.language])
+        with gr.Column(scale=1):
+            link = gr.Markdown(texts[5][predict.language])
+    with gr.Row():
+        with gr.Column(scale=100000):
             desc = gr.Markdown(texts[1][predict.language])
         with gr.Column(scale=1):
             top_k = gr.Dropdown(value=top_k, label="Top-K Concepts",
-                                choices=[i for i in range(5, max_k + 1, 5)], multiselect=False)
+                                choices=[i for i in range(5, max_k + 1, 5)],
+                                multiselect=False,
+                                min_width=10)
             top_k.change(predict.change_top_k, inputs=[top_k])
         with gr.Column(scale=1):
             bottom_k = gr.Dropdown(value=bottom_k, label="Bottom-K Concepts",
-                                   choices=[i for i in range(5, max_k + 1, 5)], multiselect=False)
+                                   choices=[i for i in range(5, max_k + 1, 5)], multiselect=False,
+                                   min_width=10)
             bottom_k.change(predict.change_bottom_k, inputs=[bottom_k])
         with gr.Column(scale=1):
             lan = gr.Dropdown(label="Language", value='en', choices=["en", "cn"], elem_id="language",
-                              multiselect=False)
+                              multiselect=False,
+                              min_width=10)
             lan.change(update_texts, inputs=[lan], outputs=[title, desc, btn_predict, btn_intervene, btn_report])
-
     with gr.Row():
         with gr.Accordion("Image Examples, Click to apply", open=True, elem_id="input-panel"):
             gr.Examples(
